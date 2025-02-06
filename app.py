d8b1c37d87af9010ca434faf1afe4cb836df4b5c
yogesh7sanap <60127449+yogesh7sanap@users.noreply.github.com>
2025-02-06 11:42:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import sklearn
 
+# Download NLTK corpora
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
 #changing background color
 # st.markdown("""
 #     <style>
